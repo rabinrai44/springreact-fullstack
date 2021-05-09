91cbfb78dca72206becc1e62962305fe92df7ab7
Rabin Rai <rabinrai44@gmail.com>
2021-05-09 14:57:41 -0400

    Initial commit

---
+++ src/main/java/com/abc/springreactfullstack/SpringreactFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.abc.springreactfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abc/springreactfullstack/SpringreactFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.springreactfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringreactFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
